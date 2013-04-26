b7f5c3991bc44427fb6494b56b76982aaf67daa3
Daniel Little <daniel@icemedia.com.au>
2013-04-26 12:13:55 +1000

    Initial commit

---
+++ SpeedTest/Global.asax.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace SpeedTest
+{
+	// Note: For instructions on enabling IIS6 or IIS7 classic mode, 
+	// visit http://go.microsoft.com/?LinkId=9394801
+	public class MvcApplication : System.Web.HttpApplication
+	{
+		//using System.Diagnostics;
+		//Stopwatch sw = new Stopwatch();
+
+		public MvcApplication()
+			: base()
+		{
+			var task = new Task(() => {
+				AreaRegistration.RegisterAllAreas();
+
+				WebApiConfig.Register(GlobalConfiguration.Configuration);
+				FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+				RouteConfig.RegisterRoutes(RouteTable.Routes);
+
+			});
+
+			this.BeginRequest += MvcApplication_BeginRequest;
+		}
+
+		void MvcApplication_BeginRequest(object sender, EventArgs e)
+		{
+			Response.ContentType = "text/html";
+			Response.WriteFile(Server.MapPath(@"~\Cache\default.html"));
+			Response.End();
+			Response.Close();
+		}
+
+
+	}
+}
---
+++ SpeedTest/Timing.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SpeedTest
+{
+	public class Timing
+	{
+		public string Name { get; set; }
+		public DateTime Time { get; set; }
+	}
+}
